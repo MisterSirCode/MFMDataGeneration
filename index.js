7dbc95c8283f07acc5d8073e6d26a17ee7632575
Sir Code <taylorschneider07@yahoo.com>
2022-07-23 23:21:21 -0400

    update to support lang file

--- index.js
+++ index.js
@@ -54,20 +54,25 @@ function RecipeJSON(type, id) {
 }
 
 function GenerateWoodModels() {
+    let listOfData = {};
     furnitures.forEach(type => {
         vanillaLogs.forEach(wood => {
+            let fid = `${wood}_${type}`;
+            listOfData[`block.mfm_utils.${fid}`] = fid.split("_").join(" ").toUpperCase();
             let stem = false;
             if (wood.endsWith("warped") || wood.endsWith("crimson")) stem = true; 
             let model = JSON.stringify(ModelJSON(type, wood, stem), null, 4);
             let state = JSON.stringify(BlockstateJSON(wood + "_" + type), null, 4);
             let recip = JSON.stringify(RecipeJSON(type, wood), null, 4);
-            fs.writeFileSync(`./resources/assets/mfm_utils/models/block/${wood}_${type}.json`, model);
-            fs.writeFileSync(`./resources/assets/mfm_utils/models/item/${wood}_${type}.json`, model);
-            fs.writeFileSync(`./resources/assets/mfm_utils/models/item/${wood}_${type}.json`, model);
-            fs.writeFileSync(`./resources/assets/mfm_utils/blockstates/${wood}_${type}.json`, state);
-            fs.writeFileSync(`./resources/data/mfm_utils/recipes/${wood}_${type}.json`, recip);
+            fs.writeFileSync(`./resources/assets/mfm_utils/models/block/${fid}.json`, model);
+            fs.writeFileSync(`./resources/assets/mfm_utils/models/item/${fid}.json`, model);
+            fs.writeFileSync(`./resources/assets/mfm_utils/models/item/${fid}.json`, model);
+            fs.writeFileSync(`./resources/assets/mfm_utils/blockstates/${fid}.json`, state);
+            fs.writeFileSync(`./resources/data/mfm_utils/recipes/${fid}.json`, recip);
         });
-    })
+    });
+    let langf = JSON.stringify(listOfData, null, 4);
+    fs.writeFileSync(`./resources/assets/mfm_utils/lang/en_us.json`, langf);
 }
 
 GenerateWoodModels();
